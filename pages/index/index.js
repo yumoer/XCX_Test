29484245df477d87e07a6f84575a075bbd1df5b4
yumo728 <794267701@qq.com>
2018-05-24 23:01:04 +0800

    first wx_xcx

---
+++ pages/index/index.js
@@ -0,0 +1,89 @@
+// pages/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:'倾覆韶华',
+    userInfo:{}
+  },
+
+  handleClick() {
+    //console.log('父元素');
+    //跳转页面
+    wx.redirectTo({
+      url: '/pages/list/list',
+      success(){
+        console.log('跳转成功');
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log('页面加载完毕');
+    console.log(this);
+    //开启定时器，发送ajax请求
+    //获取登陆用户信息
+    wx.getUserInfo({
+      success:(data) => {
+        console.log(data);
+        //更新数据
+        this.setData({userInfo:data.userInfo});
+      }
+    })
+  },
+  
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
